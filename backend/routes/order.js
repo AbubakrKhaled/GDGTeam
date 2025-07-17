a8a8abbeb0d27ec290c6235ec0df67250eea749f
Abubakr <abubakr.bedda@gmail.com>
2025-07-17 07:04:13 +0300

    Order routes

---
+++ backend/routes/order.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getCartDetails,
+    getOrderById,
+    getOrders,
+    updateOrderStatus,
+    deactivateOrder,
+    checkoutOrder
+
+} = require('../controllers/order.js');
+const { customerAuth, adminAuth, brandAuth } = require('../middlewares/auth.js');
+const adminController = require('../controllers/admin');
+const productController = require('../controllers/product');
+
+router.get('/', adminAuth, brandAuth, customerAuth, getOrders);
+router.get('/:id', adminAuth, brandAuth, customerAuth, getOrderById);
+
+router.put('/status', adminAuth, brandAuth, updateOrderStatus);
+router.put('cancel', adminAuth, brandAuth, customerAuth, deactivateOrder);
+
+router.post('/checkout', customerAuth, checkoutOrder);
+
+
+
+
+
+module.exports = router;
